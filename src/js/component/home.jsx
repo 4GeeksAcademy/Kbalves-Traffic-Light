37c8a0b017e446135758de44ea8b97b10b153e7a
Kaio Alves <118333661+Kbalves@users.noreply.github.com>
2023-08-29 00:53:23 +0000

    Kaio Traffic Light

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const TrafficLight = () => {
+  const [red, setRed] = useState(false);
+  const [yellow, setYellow] = useState(false);
+  const [green, setGreen] = useState(false);
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const handleRedClick = () => {
+    setRed(true);
+    setYellow(false);
+    setGreen(false);
+  };
+
+  const handleYellowClick = () => {
+    setRed(false);
+    setYellow(true);
+    setGreen(false);
+  };
+
+  const handleGreenClick = () => {
+    setRed(false);
+    setYellow(false);
+    setGreen(true);
+  };
+
+  return (
+    <div
+      style={{
+        backgroundColor: "black",
+        width: "80px",
+        height: "180px",
+        borderRadius: "5px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "10px",
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: red ? "red" : "darkorange",
+          width: "50px",
+          height: "50px",
+          borderRadius: "50%",
+          cursor: "pointer",
+          boxShadow: red ? "0 0 10px red" : "none",
+        }}
+        onClick={handleRedClick}
+      ></div>
+      <div
+        style={{
+          backgroundColor: yellow ? "yellow" : "yellow",
+          width: "50px",
+          height: "50px",
+          borderRadius: "50%",
+          cursor: "pointer",
+          boxShadow: yellow ? "0 0 10px yellow" : "none",
+        }}
+        onClick={handleYellowClick}
+      ></div>
+      <div
+        style={{
+          backgroundColor: green ? "green" : "lightgreen",
+          width: "50px",
+          height: "50px",
+          borderRadius: "50%",
+          cursor: "pointer",
+          boxShadow: green ? "0 0 10px green" : "none",
+        }}
+        onClick={handleGreenClick}
+      ></div>
+    </div>
+  );
 };
 
-export default Home;
+export default TrafficLight;
